47afa031998f43d52289427388fc411aa19de554
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2021-12-09 12:45:13 +0100

    con reload function

--- js/script.js
+++ js/script.js
@@ -3,48 +3,61 @@
 let kM=0;
 let eta=0;
 
-const start=document.getElementById("calcola").addEventListener("click",function(){
-
-kM=document.getElementById("kmValue").value;
-eta=document.getElementById("ageValue").value;
-// devo aggiungere una funzione che controlla l'età
-let prezzo = kM * 0.21;
-let sconto=0;
+const start=document.getElementById("calcola");
+const restart=document.getElementById("restart");
 
 
-console.log(`chilometri da percorrere : ${kM}`);
-console.log(`età della persona : ${eta}`);
+start.addEventListener("click",function(){
 
-console.log(prezzo+" "+"euro");
+    kM=document.getElementById("kmValue").value;
+    eta=document.getElementById("ageValue").value;
 
-if(eta<=18){
-    sconto=(prezzo/100)*20;
-    prezzo=prezzo-sconto;
-    console.log("sconto minorenni");
 
-    console.log(`${kM*2.1} euro - ${sconto.toFixed(2)} = ${prezzo.toFixed(2)} euro`);
+    // devo aggiungere una funzione che controlla l'età
+    let prezzo = kM * 0.21;
+    let sconto=0;
 
-    //stampo in pagina 
-    document.getElementById("prezzoContainer").innerHTML=
-    `${kM*2.1} euro - sconto minorenni =  ${sconto.toFixed(2)} = ${prezzo.toFixed(2)} euro`;
 
-}else if(eta>=65){
+    console.log(`chilometri da percorrere : ${kM}`);
+    console.log(`età della persona : ${eta}`);
+    console.log(prezzo+" "+"euro");
 
-    
-    sconto=(prezzo/100)*40;
-    prezzo=prezzo-sconto;
-    console.log("sconto anziani");
 
-    console.log(`${kM*2.1} euro - ${sconto.toFixed(2)} = ${prezzo.toFixed(2)} euro`);
 
-     //stampo in pagina 
-     document.getElementById("prezzoContainer").innerHTML=
-     `${kM*2.1} euro - sconto anziani =  ${sconto.toFixed(2)} = ${prezzo.toFixed(2)} euro`;
-}else{
+    if(eta<=18){
+        sconto=(prezzo/100)*20;
+        prezzo=prezzo-sconto;
+        console.log("sconto minorenni");
 
-    // stampo in pagina prezzo senza nessuno sconto 
-    document.getElementById("prezzoContainer").innerHTML=
-    `totale da pagare ${kM * 0.21} euro`;
-}
+        console.log(`${kM*2.1} euro - ${sconto.toFixed(2)} = ${prezzo.toFixed(2)} euro`);
+
+        //stampo in pagina 
+        document.getElementById("prezzoContainer").append
+        (`${kM*2.1} euro - sconto minorenni =  ${sconto.toFixed(2)} = ${prezzo.toFixed(2)} euro`);
+         
+    }else if(eta>=65){
+
+        
+        sconto=(prezzo/100)*40;
+        prezzo=prezzo-sconto;
+        console.log("sconto anziani");
+
+        console.log(`${kM*2.1} euro - ${sconto.toFixed(2)} = ${prezzo.toFixed(2)} euro`);
+
+        //stampo in pagina 
+        document.getElementById("prezzoContainer").append
+        (`${kM*2.1} euro - sconto anziani =  ${sconto.toFixed(2)} = ${prezzo.toFixed(2)} euro`);
+    }else{
+
+        // stampo in pagina prezzo senza nessuno sconto 
+        document.getElementById("prezzoContainer").append
+        (`totale da pagare ${kM * 0.21} euro`);
+    }
 
 });
+
+restart.addEventListener("click",function(){
+    console.log("restart");
+    location.reload();
+     
+})
